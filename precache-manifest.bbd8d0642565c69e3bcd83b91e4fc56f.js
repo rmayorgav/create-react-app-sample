e18febee3db28c2e8ee5e43315af8271f01260fd
Rodrigo Mayorga Vallejos <hello@rmayorgav.com>
2020-08-25 22:13:13 -0400

    Updates

---
+++ precache-manifest.bbd8d0642565c69e3bcd83b91e4fc56f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e552c431981c960171e71553489b88d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef5ed6f360dc271c72bc",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2f4d1a806aa80c805dcb",
+    "url": "/static/js/2.147378f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.147378f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef5ed6f360dc271c72bc",
+    "url": "/static/js/main.9f8e9e60.chunk.js"
+  },
+  {
+    "revision": "f8752028178a0bb43a49",
+    "url": "/static/js/runtime-main.76ff3aaf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
